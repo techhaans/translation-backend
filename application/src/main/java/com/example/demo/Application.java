31454786d2b2e759d06aa268e0f986dd62624ff3
nookan <nookaraju.kandregula@kambi.com>
2025-04-07 12:19:37 +0530

    initial commit with all new modules

--- application/src/main/java/com/example/demo/Application.java
+++ application/src/main/java/com/example/demo/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TranslationBackendApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TranslationBackendApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
---
+++ dc/readme.md
@@ -0,0 +1,4 @@
+Here you can add modules like
+api
+facade
+facadeimpl
